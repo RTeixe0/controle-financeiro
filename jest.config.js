59fc06e4ca85894705a8da462f27c12e78aa86db
Renan Teixeira <126295997+RTeixe0@users.noreply.github.com>
2025-06-07 16:25:35 -0300

    Add basic jest setup and sample test (#34)

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transform: {
+    '^.+\\.(t|j)sx?$': ['ts-jest', { useESM: true }],
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
   testMatch: ['**/__tests__/**/*.(test|spec).[jt]s?(x)'],
-  testPathIgnorePatterns: ['/node_modules/']
+  testPathIgnorePatterns: ['/node_modules/'],
 }
