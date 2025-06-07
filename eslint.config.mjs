04a7bb478e734306b95ad7adc716f893fdf1a827
Renan Teixeira <126295997+RTeixe0@users.noreply.github.com>
2025-06-07 14:54:34 -0300

    chore: configure eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,13 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { createRequire } from 'module';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const require = createRequire(import.meta.url);
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  ...compat.config(require('./.eslintrc.json')),
 ];
-
-export default eslintConfig;
